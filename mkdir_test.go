08c0489308e43f9baf0808fef2d19908baabb7f1
Takuya Mannami <takuya.mannami@gmail.com>
2023-02-19 13:38:44 +0900

    add test

---
+++ mkdir_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func Test_mkdir(t *testing.T) {
+	_, err := mkdir("mkdir mkdir_test")
+	defer rm("mkdir_test")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	fi, err := os.Stat("mkdir_test")
+	if err != nil {
+		t.Error(err)
+	}
+	if fi.Name() != "mkdir_test" {
+		t.Errorf("got %s, want test", fi.Name())
+	}
+}
