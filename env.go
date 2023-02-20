39fb1984e976cb89ef7324e7be675a11434be470
Takuya Mannami <takuya.mannami@gmail.com>
2023-02-20 20:50:35 +0900

    v1.1.18

--- env.go
+++ env.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func env() (string, error) {
+	envs := os.Environ()
+	var sb strings.Builder
+
+	for _, v := range envs {
+		fmt.Fprintln(&sb, v)
+	}
+
+	return strings.TrimRight(sb.String(), "\n"), nil
+}
