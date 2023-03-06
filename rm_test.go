64b0ea41e8f4fa8b32c5e9daf0bfaa574e2f9390
Takuya Mannami <takuya.mannami@gmail.com>
2023-03-06 21:43:48 +0900

    modify rm_test.go

--- rm_test.go
+++ rm_test.go
@@ -1,10 +1,28 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func Test_rm(t *testing.T) {
 	_, err := rm("rm")
 	if err == nil {
 		t.Errorf("got true, want false")
+	}
+
+	_, err = touch("touch test.txt")
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = rm("rm test.txt")
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = os.Stat("test.txt")
+	if err == nil {
+		t.Error("rm error")
 	}
 }
