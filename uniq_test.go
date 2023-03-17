19f9a21feac06e631dbf8afceedd6d5b6e278f24
Takuya Mannami <takuya.mannami@gmail.com>
2023-03-17 18:04:21 +0900

    modify uniq_test.go

--- uniq_test.go
+++ uniq_test.go
@@ -5,8 +5,12 @@ import (
 )
 
 func Test_uniq(t *testing.T) {
-	_, err := uniq("uniq a\nb\nc\n")
+	result, err := uniq("uniq a\nb\nc\nc")
 	if err != nil {
 		t.Error(err)
+	}
+
+	if result != "a\nb\nc" {
+		t.Error("uniq error")
 	}
 }
